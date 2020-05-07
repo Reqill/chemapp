fef53b7561c122262a16f9642b773d0f312be1c9
Mikołaj Mrózek <31847808+Reqill@users.noreply.github.com>
2020-05-07 14:17:17 +0200

    Add files via upload

---
+++ precache-manifest.97e7f59bef9b579d792e5af8c48792b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59f5471716e89ac042d71e54b6924ef2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87b55bb5a9c93dcc9773",
+    "url": "/static/css/main.9d85009a.chunk.css"
+  },
+  {
+    "revision": "bb467d6a3999c6857f1e",
+    "url": "/static/js/2.3f7b0b51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3f7b0b51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87b55bb5a9c93dcc9773",
+    "url": "/static/js/main.2ba45497.chunk.js"
+  },
+  {
+    "revision": "04eba7b09e63e7864347",
+    "url": "/static/js/runtime-main.bbb1cd8e.js"
+  }
+]);
